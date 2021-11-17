ed71fd945b46a88fc8cb6e576e111cdc2bddbfc4
Kaplin Sergey <kaplin@internet.ru>
2021-11-17 23:32:28 +0300

    выполнил задание 2

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,13 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+       float firstBoxer = 78.2f;
+       float secondBoxer = 82.7f;
+
+       float sumBoxers = firstBoxer + secondBoxer;
+       float weightDifference = secondBoxer % firstBoxer ;
+       System.out.println("Общий вес двух бойцов = " + sumBoxers);
+       System.out.println("Разница между весами бойцов = " + weightDifference);
     }
 }
